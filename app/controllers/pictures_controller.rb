7d7619ce5a4d88e63a872be56c716e0206050373
Joe <jpalumb0@chicagobooth.edu>
2017-07-20 00:58:36 +0000

    routes created
    most routes empty

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,7 @@ class PicturesController < ApplicationController
     end
     
     def create_row
+        
         render("pic_templates/create_row.html.erb")
     end
     
@@ -16,5 +17,20 @@ class PicturesController < ApplicationController
        @pic = Photo.find(params["an_id"])
        
        render("pic_templates/show.html.erb")
+    end
+    
+    def edit_form
+        
+        render("pic_templates/edit_form.html.erb")
+    end
+    
+    def update_row
+        
+        render("pics_template/update.html.erb")
+    end
+    
+    def destroy_row
+        
+        render("pics_template/destroy.html.erb")
     end
 end
